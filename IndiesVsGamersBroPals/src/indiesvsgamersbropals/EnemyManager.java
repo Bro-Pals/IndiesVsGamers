f9fe30b1c703f2e6cda9ebba71c70c35a68ef639
Kevin Prehn <kprehn01@gmail.com>
2015-07-17 01:55:50 -0600

    Placeholder methods

--- IndiesVsGamersBroPals/src/indiesvsgamersbropals/EnemyManager.java
+++ IndiesVsGamersBroPals/src/indiesvsgamersbropals/EnemyManager.java
@@ -6,10 +6,33 @@
 
 package indiesvsgamersbropals;
 
+import indiesvsgamersbropals.entity.SwordEntity;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author Kevin
  */
 public class EnemyManager {
 
+    HashMap<Point, ArrayList<SwordEntity>> enemiesSave;
+    
+    /**
+     * Save the enemy to the enemies
+     * @param sceneX The secene's y position
+     * @param sceneY The scene's x position
+     * @param enemy The enemy to save
+     */
+    public void saveEnemy(int sceneX, int sceneY, SwordEntity enemy) {
+        
+    }
+    
+    public ArrayList<SwordEntity> getEnemies(int sceneX, int sceneY) {
+        ArrayList<SwordEntity> entities = new ArrayList<>();
+        
+        return entities;
+    }
+    
 }
